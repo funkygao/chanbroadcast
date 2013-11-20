655510d2052b49a9ebdecf9d3aaff0ee8c6c01a5
funkygao <funky.gao@gmail.com>
2013-11-20 22:31:55 +0800

    pkg doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+The broadcast channel is created with NewBroadcaster, and values can be written to it with Write. 
+To listen on the channel, we call Subscribe, which gives us a new channel from which we can 
+receive the values written.
+*/
+package chanbroadcast
